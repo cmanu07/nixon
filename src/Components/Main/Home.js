2aa55db136cdc618699472d4b5587b19b8cac715
cmanu07 <cemanuel86@gmail.com>
2023-10-15 20:23:02 +0300

    final updates #6

--- src/Components/Main/Home.js
+++ src/Components/Main/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Link } from 'react-scroll';
+import Spline from '@splinetool/react-spline'
+import { Link } from 'react-scroll'
 
-import Spline from '@splinetool/react-spline';
 
 import './Styles/Home.css'
 
